4c4fbe2ba4c7c0e870b39ad0b7ecdbe1389bce97
Ankit Agarwal <ankit@moodle.com>
2012-08-10 15:57:56 +0800

    advancedspamcleaner: Updates to the akismet plugin

--- plugins/akismet/api.php
+++ plugins/akismet/api.php
@@ -5,9 +5,13 @@ require_once('akismet.class.php');
 class akismet_advanced_spam_cleaner extends base_advanced_spam_cleaner {
     function detect_spam ($data) {
         global $CFG;
-        $APIKey = 'xxxxxxx';
 
-        $akismet = new Akismet($CFG->wwwurl ,$APIKey);
+        $apikey = get_config('advancedspamcleaner', 'akismetkey');
+        if (!$apikey) {
+            print_error("Set api key from settings before using this option");
+        }
+
+        $akismet = new Akismet($CFG->wwwroot, $apikey);
         $akismet->setCommentAuthorEmail($data->email);
         $akismet->setCommentContent($data->text);
         $akismet->setUserIP($data->ip);
