8706d9e84f15a96f2dec4ef2a500fc2e3bf41caa
Ankit Agarwal <ankit@moodle.com>
2013-11-13 12:01:42 +0800

    Stop support for Moodle 2.3

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013111300;
-$plugin->requires  = 2012062500;
+$plugin->requires  = 2012120300;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = "2.0";
 $plugin->component = 'tool_advancedspamcleaner'; // Full name of the plugin (used for diagnostics).
